0db2ae4c49a3d5e2af20f6f38aeb15c918e9c5af
Alexey <alexey.podobed@gmail.com>
2014-11-17 17:55:05 +0100

    add service

--- front-end/app/scripts/services/authtoken.js
+++ front-end/app/scripts/services/authtoken.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('fullStackApp')
-  .factory('authToken', function () {
-
+  .factory('authToken', function ($window) {
+    var storage = $window.localStorage,
+        cachedToken;
 
     return {
-
+      setToken: function(token){
+          cachedToken = token;
+          storage.setItem('userToken', token);
+      },
+      getToken: function(){
+        if(!cachedToken){
+          cachedToken = storage.getItem('userToken');
+        }
+        return cachedToken;
+      },
+      isAuthentificate: function(){
+        return !!this.getToken();
+      }
     };
   });
